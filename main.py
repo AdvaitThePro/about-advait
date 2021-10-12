a81704106d7193ff3de6bdd31da8ce4cbd0893d5
Milind Contractor <ai.dotproduct@gmail.com>
2021-10-12 22:24:18 +0800

    User Interface overhaul for 'Our Services'

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('new index.html')
+
+@app.route('/secser')
+def secser():
+    return render_template('secser.html')
+
+@app.route('/accser')
+def accser():
+    return render_template('accser.html')
+
+@app.route('/taxser')
+def taxser():
+    return render_template('taxser.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
